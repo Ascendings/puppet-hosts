1f20b0cfbf0f3d7c5a1a3801d1085433c7ab19ab
Ascendings <brotherballantine@gmail.com>
2015-08-06 16:24:07 -0400

    flipped around if statement in config.pp to check for false

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,21 +1,21 @@
 # See README.md for details.
 class hosts::config inherits ::hosts {
 
-  unless $::hosts::hosts_file_source == false {
-    file { $::hosts::hosts_file_path:
-      ensure => file,
-      owner  => $::hosts::hosts_file_owner,
-      group  => $::hosts::hosts_file_group,
-      mode   => $::hosts::hosts_file_mode,
-      source => $::hosts::hosts_file_source,
-    }
-  } else {
+  if $::hosts::hosts_file_source == false {
     file { $::hosts::hosts_file_path:
       ensure  => file,
       owner   => $::hosts::hosts_file_owner,
       group   => $::hosts::hosts_file_group,
       mode    => $::hosts::hosts_file_mode,
       content => template($::hosts::hosts_file_template),
+    }
+  } else {
+    file { $::hosts::hosts_file_path:
+      ensure => file,
+      owner  => $::hosts::hosts_file_owner,
+      group  => $::hosts::hosts_file_group,
+      mode   => $::hosts::hosts_file_mode,
+      source => $::hosts::hosts_file_source,
     }
   }
 }
